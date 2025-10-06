0b6ae73ebbc75fdbf948b4265c4359a8bd282057
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2025-10-06 17:20:29 -0300

    feat: adiciona diferenciacao entre cadastro interno e externo

--- src/backend/UsuarioExterno.js
+++ src/backend/UsuarioExterno.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const UsuarioExternoSchema = new mongoose.Schema({
     name: String,
     email: String,
-    pass: String,
     tel: String,
     dt_nascto: Date,
 })
