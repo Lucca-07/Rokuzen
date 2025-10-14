263a5a6c52ce2d8c17301271a6c7808687367d24
Diego Piol <digopiol@gmail.com>
2025-10-14 15:25:02 -0300

    fix: correcao na responsividade do circulo timer

--- src/frontend/js/cadastro.js
+++
@@ -1,9 +0,0 @@
-const userInterno = document.getElementById("containerUserInterno");
-const userExterno = document.getElementById("containerUserExterno");
-
-const selectUser = document.getElementById("usuarioselect");
-
-selectUser.addEventListener("change", () => {
-    userExterno.classList.toggle("d-none");
-    userInterno.classList.toggle("d-none");
-});
--- src/frontend/js/paginaInicio.js
+++
@@ -1,5 +0,0 @@
-const menubtn = document.getElementById("menubtn");
-function abrirMenu() {
-    const dropcnt = document.getElementById("dropcnt");
-    dropcnt.classList.toggle("d-none");
-}
